c74ecb065fb6323ed0bf9ae28f0e2ca37173a3d2
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-04-12 00:59:40 +0300

    run.sh

--- src/run.sh
+++ src/run.sh
@@ -7,13 +7,13 @@ TEND=${5:-10.0}
 HK=${6:-0.01}
 AS=${7:-1e-10}
 
-IEXT_NEG=$(( -IEXT ))
-
 if [ $CALC -eq 1 ]; then
-    echo "Calculus mode | method=$METHOD | tol=$AS | hk=$HK | Iext=$IEXT | t_end=$TEND"
-    ./hh_sim "$METHOD" "$H" "$IEXT_NEG" 1 "$TEND" "$HK" "$AS"
+    echo "Calculus mode
+    echo "method=$METHOD | h=$H | Iext=$IEXT | t_end=$TEND | tol=$AS | hk=$HK"
+    ./hh_sim "$METHOD" "$H" "$IEXT" 1 "$TEND" "$HK" "$AS"
 else
-    echo "Normal mode | method=$METHOD | h=$H | Iext=$IEXT | t_end=$TEND"
-    ./hh_sim "$METHOD" "$H" "$IEXT_NEG" 0 "$TEND" "$HK" "$AS" > "${METHOD}.dat"
-    echo "Data saved → ${METHOD}.dat"
+    echo "Normal mode"
+    echo "method=$METHOD | h=$H | Iext=$IEXT | t_end=$TEND"
+    ./hh_sim "$METHOD" "$H" "$IEXT" 0 "$TEND" "$HK" "$AS" > "${METHOD}.dat"
+    echo "Saved → ${METHOD}.dat"
 fi

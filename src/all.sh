97f3b1bff05ad7d0d220a475c2a062db98e33904
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-04-12 01:33:19 +0300

    all.sh

--- src/all.sh
+++ src/all.sh
@@ -1,13 +1,16 @@
-
 #!/bin/bash
+
 IEXT=${1:-10}
 H=${2:-0.1}
+TEND=${3:-10.0}
+HK=${4:-0.01}
+AS=${5:-1e-10}
 
-echo "Running all 3 methods | h=$H | Iext=$IEXT"
+echo "Running all 3 methods | h=$H | Iext=$IEXT | t_end=$TEND"
 
-./run.sh rk4 "$H" "$IEXT" 0 10.0 0.01 1e-14
-./run.sh dp  "$H" "$IEXT" 0 10.0 0.01 1e-10
-./run.sh mid "$H" "$IEXT" 0 10.0 0.01 1e-14
+./run.sh rk4 "$H" "$IEXT" 0 "$TEND" "$HK" "$AS"
+./run.sh dp  "$H" "$IEXT" 0 "$TEND" "$HK" "$AS"
+./run.sh mid "$H" "$IEXT" 0 "$TEND" "$HK" "$AS"
 
 ./plot.sh
 echo "Graph opened."

ee1239a2e543c6f2e8371ed7ba14042ae24dd0ac
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-04-11 23:44:11 +0300

    run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-METHOD=${1:-dp}
-H=${2:-0.01}
-IEXT=${3:-10}          # WE WRITE POSITIVE
-TOL=${4:-1e-10}
-HK=${5:-0.01}
-CALC=${6:-0}
+METHOD=$1
+H=$2
+IEXT=${3:-10}
+CALC=${4:-0}
+AS=${5:-1e-10}
+HK=${6:-0.01}
 
 IEXT_NEG=$(( -IEXT ))
 
 if [ $CALC -eq 1 ]; then
-    echo "Calculus mode"
-    ./hh_sim "$METHOD" "$H" "$IEXT_NEG" 1 "$TOL" "$HK"
+    echo "Calculus mode → $METHOD | tol=$AS | hk=$HK | Iext=$IEXT"
+    ./hh_sim "$METHOD" "$H" "$IEXT_NEG" 1 "$AS" "$HK"
 else
-    echo "Normal mode: $METHOD | h=$H | Iext=$IEXT (internal -$IEXT)"
-    ./hh_sim "$METHOD" "$H" "$IEXT_NEG" 0 "$TOL" "$HK" > "${METHOD}.dat"
+    echo "Normal mode → $METHOD | h=$H | Iext=$IEXT"
+    ./hh_sim "$METHOD" "$H" "$IEXT_NEG" 0 "$AS" "$HK" > "${METHOD}.dat"
     echo "Saved → ${METHOD}.dat"
 fi

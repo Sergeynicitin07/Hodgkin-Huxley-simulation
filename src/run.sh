6ecd811c068d3505a3f06a2c0852fb40561c7ace
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-04-16 02:13:13 +0300

    run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
+
 METHOD=${1:-dp}
-H=${2:-0.1}
+
+CALC=${2:-1}  
+
 IEXT=${3:-10}
-CALC=${4:-0}
-TEND=${5:-10.0}
-HK=${6:-0.01}
+
+TEND=${4:-10.0}
+
+H=${5:-0.1}
+
+TOL=${6:-1e-1}
+
 AS=${7:-1e-10}
 
 if [ $CALC -eq 1 ]; then
+
     echo "Calculus mode"
-    echo "method=$METHOD | h=$H | Iext=$IEXT | t_end=$TEND | tol=$AS | hk=$HK"
-    ./hh_sim "$METHOD" "$H" "$IEXT" 1 "$TEND" "$HK" "$AS"
+
+    echo "method = $METHOD | h = $H | Iext = $IEXT | t_end = $TEND | tol = $TOL | tol_for_dp = $AS"
+
+    ./hh_sim "$METHOD" "$CALC" "$IEXT" "$TEND" "$H" "$TOL" "$AS"
+
 else
+
     echo "Normal mode"
-    echo "method=$METHOD | h=$H | Iext=$IEXT | t_end=$TEND"
-    ./hh_sim "$METHOD" "$H" "$IEXT" 0 "$TEND" "$HK" "$AS" > "${METHOD}.dat"
-    echo "Saved → ${METHOD}.dat"
+
+    echo "method = $METHOD | h = $H | Iext = $IEXT | t_end = $TEND | tol = $TOL | tol_for_dp = $AS"
+
+    ./hh_sim "$METHOD" "$CALC" "$IEXT" "$TEND" "$H" "$TOL" "$AS" > "${METHOD}.dat"
+
+    echo "Data saved → ${METHOD}.dat"
+
 fi

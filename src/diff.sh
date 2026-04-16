5e1a25f9e60abf8f1d7cffde6d59cf88940e8f6e
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-04-16 15:00:32 +0300

    diff.sh

--- src/diff.sh
+++ src/diff.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-FILE1=${1:-dp.dat}
-FILE2=${2:-rk4.dat}
+FILE1=${1:-rk4.txt}
+FILE2=${2:-mid.txt}
 
 if [ ! -f "$FILE1" ] || [ ! -f "$FILE2" ]; then
-    exit 1
+
+exit 1
 fi
 
-awk 'NR==FNR {a[NR]=$2; next} {print $1, a[NR]-$2}' "$FILE1" "$FILE2" > diff.tmp
+paste "$FILE1" "$FILE2" | awk '
+{
+t = $1
+v1 = $2
+v2 = $7 # 2-й файл: t v ...
+print t, v1 - v2
+}
+' > diff.txt
+
+NAME1=$(basename "$FILE1" .txt)
+NAME2=$(basename "$FILE2" .txt)
 
 gnuplot -persist << EOF
-set multiplot layout 2,1 title "Comparison" scale 1,0.92
+set multiplot layout 2,1
 
-set title "Trajectories"
-set xlabel "Time (ms)"
-set ylabel "V (mV)"
-set grid
-plot "$FILE1" using 1:2 with lines lc rgb "blue" lw 2, \
-     "$FILE2" using 1:2 with lines lc rgb "red"  lw 2
+set title "Two solutions"
+plot "$FILE1" using 1:2 with lines lc rgb "blue" lw 2 title "$NAME1", \
+     "$FILE2" using 1:2 with lines lc rgb "red"  lw 2 title "$NAME2"
 
-set title "Difference (V1 - V2)"
-set xlabel "Time (ms)"
-set ylabel "ΔV (mV)"
-set grid
-plot "diff.tmp" using 1:2 with lines lc rgb "black" lw 2
+set title "Difference ($NAME1 - $NAME2) "
+plot "diff.txt" using 1:2 with lines lc rgb "black" lw 2 title "diff"
 
 unset multiplot
 EOF
-
-rm -f diff.tmp

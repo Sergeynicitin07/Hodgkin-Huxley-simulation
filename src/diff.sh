134bcad50b04736f2d7e22943eb588400823751c
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-04-17 12:38:47 +0300

    diff.sh

--- src/diff.sh
+++ src/diff.sh
@@ -8,7 +8,34 @@ fi
 
 rm -f diff.txt
 
-paste "$FILE1" "$FILE2" | awk '{print $1, $2 - $7}' > diff.txt
+awk '
+BEGIN {
+    while ((getline < "'"$FILE2"'") > 0) {
+        t2[++n2] = $1
+        v2[n2] = $2
+    }
+    close("'"$FILE2"'")
+}
+{
+    t1 = $1
+    v1 = $2
+
+    if (t1 < t2[1] || t1 > t2[n2]) {
+        printf "Warning: t=%.6f out of range [%.6f, %.6f] WE SHOULD DIED!!!\n", t1, t2[1], t2[n2] > "/dev/stderr"
+        next
+    }
+
+    lo = 1; hi = n2
+    while (hi - lo > 1) {
+        mid = int((lo + hi) / 2)
+        if (t2[mid] <= t1) lo = mid
+        else hi = mid
+    }
+
+    v2_interp = v2[lo] + (v2[hi] - v2[lo]) * (t1 - t2[lo]) / (t2[hi] - t2[lo])
+    diff = v1 - v2_interp
+    print t1, diff
+}' "$FILE1" > diff.txt
 
 NAME1=$(basename "$FILE1" .txt)
 NAME2=$(basename "$FILE2" .txt)
@@ -23,7 +50,7 @@ set grid
 plot "$FILE1" using 1:2 with lines lc rgb "blue" lw 2 title "$NAME1", \
      "$FILE2" using 1:2 with lines lc rgb "red"  lw 2 title "$NAME2"
 
-set title "Difference ($NAME1 - $NAME2)"
+set title "Difference ($NAME1 - $NAME2) interpolated on $NAME1 grid"
 set xlabel "Time (ms)"
 set ylabel "ΔV (mV)"
 set grid

173f67d6f64b963f0bc35a0909af807f7e69b144
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-04-18 16:31:31 +0300

    diff.sh

--- src/diff.sh
+++ src/diff.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
+
 FILE1=${1:-rk4.txt}
 FILE2=${2:-mid.txt}
 
 if [ ! -f "$FILE1" ] || [ ! -f "$FILE2" ]; then
+    echo "WE SHOULD DIE!"
     exit 1
 fi
 
-rm -f diff.txt
-
 awk '
-BEGIN {
-    while ((getline < "'"$FILE2"'") > 0) {
-        t2[++n2] = $1
-        v2[n2] = $2
+    NR==FNR { 
+        t2[NR] = $1; 
+        v2[NR] = $2; 
+        n2 = NR; 
+        next 
     }
-    close("'"$FILE2"'")
-}
-{
-    t1 = $1
-    v1 = $2
-
-    if (t1 < t2[1] || t1 > t2[n2]) {
-        printf "Warning: t=%.6f out of range [%.6f, %.6f] WE SHOULD DIED!!!\n", t1, t2[1], t2[n2] > "/dev/stderr"
-        next
+    {
+        t1 = $1
+        v1 = $2
+        lo = 1; hi = n2
+        while (hi - lo > 1) {
+            mid = int((lo + hi)/2)
+            if (t2[mid] <= t1) lo = mid
+            else hi = mid
+        }
+        if (t1 <= t2[1])      v2_interp = v2[1]
+        else if (t1 >= t2[n2]) v2_interp = v2[n2]
+        else {
+            frac = (t1 - t2[lo]) / (t2[lo+1] - t2[lo])
+            v2_interp = v2[lo] + frac * (v2[lo+1] - v2[lo])
+        }
+        print t1, v1 - v2_interp
     }
-
-    lo = 1; hi = n2
-    while (hi - lo > 1) {
-        mid = int((lo + hi) / 2)
-        if (t2[mid] <= t1) lo = mid
-        else hi = mid
-    }
-
-    v2_interp = v2[lo] + (v2[hi] - v2[lo]) * (t1 - t2[lo]) / (t2[hi] - t2[lo])
-    diff = v1 - v2_interp
-    print t1, diff
-}' "$FILE1" > diff.txt
+' "$FILE2" "$FILE1" > diff.txt
 
 NAME1=$(basename "$FILE1" .txt)
 NAME2=$(basename "$FILE2" .txt)
@@ -50,7 +47,7 @@ set grid
 plot "$FILE1" using 1:2 with lines lc rgb "blue" lw 2 title "$NAME1", \
      "$FILE2" using 1:2 with lines lc rgb "red"  lw 2 title "$NAME2"
 
-set title "Difference ($NAME1 - $NAME2) interpolated on $NAME1 grid"
+set title "Difference ($NAME1 - $NAME2)"
 set xlabel "Time (ms)"
 set ylabel "ΔV (mV)"
 set grid
@@ -60,3 +57,4 @@ unset multiplot
 EOF
 
 rm -f diff.txt
+echo "Done."
